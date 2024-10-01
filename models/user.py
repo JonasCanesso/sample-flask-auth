1c661393d51e34df3bce909bc55d474fd9bcbd83
JonasCanesso <akatsumo@hotmail.com>
2024-10-01 08:31:56 -0300

    correção
    correção em erros de digitação

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 from app import db
 
 class User(db.Model):
-    id = db.Column(db.interger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, Unique=True)
     password = db.Column(db.String(80), nullable=False)
